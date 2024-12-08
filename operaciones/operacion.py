7746f7764bde51faadf69a3ec381b3f44597d564
Loulou64320-cmd <pedro.lainesortega64@gmail.com>
2024-12-08 15:56:14 -0500

    operaciones

---
+++ operaciones/operacion.py
@@ -0,0 +1,24 @@
+# Aqui estan todas ka funciones
+
+def sumar(valor1, valor2):
+    return valor1 + valor2
+
+
+def restar(valor1, valor2):
+    return valor1 - valor2
+
+
+def multiplicar(valor1, valor2):
+    return valor1 * valor2
+
+
+def dividir(valor1, valor2):
+    return valor1 / valor2
+
+
+def modular(valor1, valor2):
+    return valor1 % valor2
+
+
+def exponente(valor1, valor2):
+    return valor1 ** valor2
